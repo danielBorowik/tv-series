818738b9c8e1f969236da0e7d692948f2d2686cd
danielBorowik <borowikdaniel9@gmail.com>
2020-01-06 14:56:18 +0100

    add 404 page

---
+++ .history/src/components/fourOhFour_20200106143534.vue
@@ -0,0 +1,47 @@
+<template functional>
+
+    <div class="flex-column-wrapper">
+        <h1>404 Not Found</h1>
+        <h2>The requested URL was not found on this server</h2>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+h1,
+h2 {
+  font-size: 4rem;
+  letter-spacing: 0.3rem;
+  text-shadow: 0px 0px 8px rgba(229, 9, 20, 0.81);
+  padding: 0 2rem;
+
+  @media (max-width: 522px) {
+    font-size: 3rem;
+    padding: 0 1rem;
+  }
+}
+
+h2 {
+  font-size: 2.5rem;
+  margin-top: 4rem;
+  text-align: center;
+
+  @media (max-width: 522px) {
+    font-size: 2rem;
+    padding: 0 1rem;
+  }
+}
+
+.flex-column-wrapper {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+  text-align: center;
+  overflow: hidden;
+  min-height: 100%;
+}
+
+</style>
--- .history/src/router/index_20200106143350.js
+++ .history/src/router/index_20200106143350.js
@@ -20,7 +20,7 @@ const routes = [
   {
     path: '/*',
     name: 'FourOhFour',
-    component: FourOhFour,
+    component: FourOhFour
   },
 ];
 
